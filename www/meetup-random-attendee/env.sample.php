562ec89e372ccc8608e70a6713bdef4d9bc9b683
Ben Dechrai <ben@dechrai.com>
2017-04-06 16:29:00 +1000

    Added help to get API key

--- www/meetup-random-attendee/env.sample.php
+++ www/meetup-random-attendee/env.sample.php
@@ -1,4 +1,5 @@
 <?php
 
-// copy this file to env.php
+// Copy this file to env.php, then update with your API key from https://secure.meetup.com/meetup_api/key/
+
 $meetup_api_key = 'your-api-key-here';
